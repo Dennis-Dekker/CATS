eca19c93c13a14760dfcfbbaaa845e23b0f20138
Dennis Dekker <48018095+Dennis-Dekker@users.noreply.github.com>
2019-05-10 23:37:29 +0200

    update graphs

--- scripts/graphs.py
+++ scripts/graphs.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 import math
 
 # enter raw data
-baseline = np.array([0.727, 0.809, 0.684, 0.789, 0.684])
+baseline = np.array([0.636, 0.476, 0.684, 0.684, 0.789])
+baseline_2 = np.array([0.636, 0.476, 0.684, 0.684, 0.789, 0.773, 0.619, 0.684, 0.684, 0.684, 0.636, 0.619, 0.789, 0.684, 0.789])
 anova = np.array([0.864, 0.667, 0.842, 0.895, 0.842])
 recursive = np.array([0.75, 0.85, 0.80, 0.75, 0.9])
 l1_regularization = np.array([0.773, 0.762, 0.842, 0.842, 0.842])
@@ -14,6 +15,7 @@ l1_regularization_2 = np.array([0.727, 0.762, 0.842, 0.842, 0.842, 0.727, 0.762,
 
 # calculate the average
 baseline_mean = np.mean(baseline)
+baseline_2_mean = np.mean(baseline_2)
 anova_mean = np.mean(anova)
 recursive_mean = np.mean(recursive)
 l1_regularization_mean = np.mean(l1_regularization)
@@ -22,6 +24,7 @@ l1_regularization_2_mean = np.mean(l1_regularization_2)
 
 # calculate the standard deviation
 baseline_std = np.std(baseline)
+baseline_2_std = np.std(baseline_2)
 anova_std = np.std(anova)
 recursive_std = np.std(recursive)
 l1_regularization_std = np.std(l1_regularization)
@@ -30,6 +33,7 @@ l1_regularization_2_std = np.std(l1_regularization_2)
 
 # calculate the standard error
 baseline_se = stats.sem(baseline)
+baseline_2_se = stats.sem(baseline_2)
 anova_se = stats.sem(anova)
 recursive_se = stats.sem(recursive)
 l1_regularization_se = stats.sem(l1_regularization)
@@ -50,10 +54,10 @@ print(anova_2_mean, anova_2_std, anova_2_se)
 
 
 # create lists for the plot
-methods = ['Baseline', 'ANOVA', 'ANOVA_2', 'RFS-SVM', 'L1', 'L1_2']
+methods = ['Baseline', 'Baseline_2', 'ANOVA', 'ANOVA_2', 'RFS-SVM', 'L1', 'L1_2']
 x_pos = np.arange(len(methods))
-CTEs = [baseline_mean, anova_mean, anova_2_mean, recursive_mean, l1_regularization_mean, l1_regularization_2_mean]
-error = [baseline_se, anova_se, anova_2_se, recursive_se, l1_regularization_se, l1_regularization_2_se]
+CTEs = [baseline_mean, baseline_2_mean, anova_mean, anova_2_mean, recursive_mean, l1_regularization_mean, l1_regularization_2_mean]
+error = [baseline_se, baseline_2_se, anova_se, anova_2_se, recursive_se, l1_regularization_se, l1_regularization_2_se]
 
 # build the plot
 fig, ax = plt.subplots()
